1f09aff78ef516f1c5315b246b689de52442dbd0
joselveas <joselveas@gmail.com>
2023-09-26 23:00:53 -0300

    se agregan cambios a ejemplo2

--- ejemplo2/Persona.java
+++ ejemplo2/Persona.java
@@ -1,5 +1,38 @@
 package ejemplo2;
 
-public class Persona {
-    
+interface SeComunica{
+    public void hablar();
+    public void gritar();
+}
+
+public class Persona implements SeComunica{
+    private String nombre;
+    private int edad;
+    public Persona(){
+
+    }
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+
+    public void hablar(){
+        System.out.println("La persona "+nombre+" esta hablando");   
+    }
+
+    public void gritar(){
+        System.out.println("La persona "+nombre+" esta gritando");
+    }
 }
--- ejemplo2/ejemplo.java
+++ ejemplo2/ejemplo.java
@@ -1,5 +1,24 @@
 package ejemplo2;
 
 public class ejemplo {
-    
+    public static void main(String[] args) {
+        Persona persona1 = new Adulto();
+        Persona persona2 = new Adolescente();
+
+        persona1.setNombre("Juan");
+        persona1.setEdad(20);
+        persona2.setNombre("Pedro");
+        persona2.setEdad(30);
+
+        System.out.println("Nombre: "+persona1.getNombre());
+        System.out.println("Edad: "+persona1.getEdad());
+        System.out.println("Nombre: "+persona2.getNombre());
+        System.out.println("Edad: "+persona2.getEdad());
+
+        persona1.hablar();
+        persona2.hablar();
+
+        persona1.gritar();
+        persona2.gritar();
+    }
 }
